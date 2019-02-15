837d4b85243e8ed459f142eff68c2ad6c7c1bcf9
Vikil Naik <vikilnaik37@gmail.com>
2019-02-15 16:01:22 -0500

    Starting the paperboy excercise

---
+++ paperboy.py
@@ -0,0 +1,7 @@
+class Paperboy:
+
+        # experince means the number of papers they have delivered
+    def __init__(self, name, experience, earnings):
+        self.name = name
+        self.experience = experience
+        self.earnings = earnings
